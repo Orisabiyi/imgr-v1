052db07ef91cb6ce2427638d4cc94a0e5a191905
Orisabiyi <orisabiyidavid@gmail.com>
2024-09-07 22:08:51 +0100

    chore: Update image upload UI and styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,16 +17,27 @@ function App() {
 
   return (
     <main className="flex items-center justify-center h-screen bg-gray-200">
-      <section className="bg-white w-[25%] py-[4rem] px-[2rem] flex flex-col items-center justify-center rounded-[1rem]">
-        <h1>Upload your image</h1>
-        <p>File should be jpeg, png, ....</p>
+      <section className="bg-white w-[85%] sm:w-[60%] lg:w-[30%] py-[4rem] px-[2rem] flex flex-col items-center justify-center gap-[1rem] md:gap-[2rem] rounded-[1rem]">
+        <h1 className="text-[2rem]">Upload your image</h1>
+        <p className="text-[1.4rem] mb-[1rem] lg:text-[1.2rem]">
+          File should be jpeg, png, ....
+        </p>
 
-        <div className="flex items-center justify-center">
-          <img src="/photos.png" alt="" className="h-[4.6rem] w-auto" />
+        <div className="flex flex-col items-center justify-center gap-[2rem] p-[4rem] bg-blue-200/10 rounded-[1.5rem] w-full outline-dashed outline-2 outline-slate-400/30">
+          <img src="/photos.png" alt="" className="h-[10rem] w-auto" />
+          <p className="text-[1.5rem] text-gray-400">
+            Drag & Drop your image here
+          </p>
         </div>
 
         <p>Or</p>
-        <button onClick={handleClick}>Choose a file</button>
+        <button
+          onClick={handleClick}
+          className="bg-blue-600 text-white text-[1.4rem] px-7 py-4 rounded-xl"
+        >
+          Choose a file
+        </button>
+
         <input
           type="file"
           ref={imageFile}
