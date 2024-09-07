4196dd3d9365529e9b236e4ad79a5a300f92be02
Orisabiyi <orisabiyidavid@gmail.com>
2024-09-07 22:45:48 +0100

    chore: Refactor image upload functionality and UI styling

---
+++ frontend/src/pages/Homepage.jsx
@@ -0,0 +1,58 @@
+import { useRef, useState } from "react";
+
+function Homepage() {
+  const imageFile = useRef(null);
+  const [uploadImg, setUploadImg] = useState(null);
+
+  function handleClick() {
+    imageFile.current.click();
+  }
+
+  function handleFileChange(e) {
+    const file = e.target.files[0];
+    if (file.type !== "image/jpeg" && file.type !== "image/png") return;
+
+    const imageUrl = URL.createObjectURL(file);
+    setTimeout(() => setUploadImg(imageUrl), 2000);
+  }
+
+  return (
+    <main className="flex items-center justify-center h-screen bg-gray-200">
+      <section className="bg-white w-[85%] sm:w-[60%] lg:w-[30%] py-[4rem] px-[2rem] flex flex-col items-center justify-center gap-[1rem] md:gap-[2rem] rounded-[1rem]">
+        <h1 className="text-[2rem]">Upload your image</h1>
+        <p className="text-[1.4rem] mb-[1rem] lg:text-[1.2rem]">
+          File should be jpeg, png, ....
+        </p>
+
+        <div className="flex flex-col items-center justify-center gap-[2rem] p-[4rem] bg-blue-200/10 rounded-[1.5rem] w-full outline-dashed outline-2 outline-slate-400/30">
+          <img
+            src="/photos.png"
+            alt="image placeholder"
+            className="h-[10rem] w-auto"
+          />
+          <p className="text-[1.5rem] text-gray-400">
+            Drag & Drop your image here
+          </p>
+        </div>
+
+        <p className="text-[1.4rem] lg:text-[1.2rem]">Or</p>
+
+        <button
+          onClick={handleClick}
+          className="bg-blue-600 text-white text-[1.4rem] px-7 py-4 rounded-xl"
+        >
+          Choose a file
+        </button>
+
+        <input
+          type="file"
+          ref={imageFile}
+          onChange={handleFileChange}
+          className="hidden"
+        />
+      </section>
+    </main>
+  );
+}
+
+export default Homepage;
