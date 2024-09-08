630e41a9cab2e9bd11b9a998e01d3559648378f4
Orisabiyi <orisabiyidavid@gmail.com>
2024-09-08 05:40:05 +0100

    Refactor Loading component to use navigate hook for redirecting after upload

--- frontend/src/pages/Loading.jsx
+++ frontend/src/pages/Loading.jsx
@@ -1,16 +1,20 @@
+import { useNavigate } from "react-router-dom";
 import { ImgContext } from "../contexts/ImgContext";
 import { BarLoader } from "react-spinners";
 
 function Loading() {
+  const navigate = useNavigate("");
   const { isLoading } = ImgContext();
 
   return (
     <main className="h-screen flex items-center justify-center bg-gray-200">
-      {isLoading && (
+      {isLoading ? (
         <section className="rounded-xl px-[2rem] py-[3rem] shadow-md bg-white w-[85%] sm:w-[40%] lg:w-[30%]">
-          <h1 className="text-[1.6rem] mb-[1rem]">Uploading...</h1>
+          <h1 className="text-[1.6rem] mb-[2rem]">Uploading...</h1>
           <BarLoader color="#2563eb" data-testid="loader" />
         </section>
+      ) : (
+        navigate("/uploaded")
       )}
     </main>
   );
