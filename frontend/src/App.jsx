99202bd423a827ea06aa504a1ad29985bf333717
Orisabiyi <orisabiyidavid@gmail.com>
2024-09-08 05:07:30 +0100

    feat: Refactor App component to remove unnecessary ImgContext dependency and improve routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
-import { ImgContext, ImgContextProvider } from "./contexts/ImgContext";
+import { ImgContextProvider } from "./contexts/ImgContext";
 import Loading from "./pages/Loading";
 
 function App() {
-  const { isLoading } = ImgContext();
-
   return (
     <BrowserRouter>
       <ImgContextProvider>
         <Routes>
           <Route path="/" element={<Homepage />} />
-          {isLoading && <Route path="/uploading" element={<Loading />} />}
+          <Route path="/uploading" element={<Loading />} />
         </Routes>
       </ImgContextProvider>
     </BrowserRouter>
