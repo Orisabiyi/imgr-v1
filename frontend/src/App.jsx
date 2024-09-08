08b9015b2588a600a273af1d15d6538c601d236c
Orisabiyi <orisabiyidavid@gmail.com>
2024-09-08 05:41:37 +0100

    feat: Add UploadPage component and route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import { ImgContextProvider } from "./contexts/ImgContext";
 import Loading from "./pages/Loading";
+import UploadPage from "./pages/UploadPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/uploading" element={<Loading />} />
+          <Route path="/uploaded" element={<UploadPage />} />
         </Routes>
       </ImgContextProvider>
     </BrowserRouter>
