df60a59ef9acb640d718fb2ac9265dbedfe642e8
Orisabiyi <orisabiyidavid@gmail.com>
2024-09-08 05:20:52 +0100

    Refactor Loading component to use isLoading state from ImgContext

--- frontend/src/pages/Loading.jsx
+++ frontend/src/pages/Loading.jsx
@@ -5,10 +5,11 @@ function Loading() {
   const { isLoading } = ImgContext();
 
   return (
-    <main>
+    <main className="h-screen flex items-center justify-center bg-gray-200">
       {isLoading && (
-        <section>
-          <BarLoader />
+        <section className="rounded-xl px-[2rem] py-[3rem] shadow-md bg-white w-[85%] sm:w-[40%] lg:w-[30%]">
+          <h1 className="text-[1.6rem] mb-[1rem]">Uploading...</h1>
+          <BarLoader color="#2563eb" data-testid="loader" />
         </section>
       )}
     </main>
