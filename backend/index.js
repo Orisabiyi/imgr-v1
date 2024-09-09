6a2c35593149e570b556f9742b5ef348907c5acf
Orisabiyi <orisabiyidavid@gmail.com>
2024-09-09 21:02:29 +0100

    Refactor MongoDB connection string in backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ app.get("/", (req, res) => res.status(200).json({ message: "Hello World" }));
 
 mongoose
   .connect(
-    `mongodb+srv://orisabiyidavid:${process.env.DB_PASSWORD}@img-data.n7vgx.mongodb.net/img-data?retryWrites=true&w=majority&appName=img-data`
+    `mongodb+srv://orisabiyidavid:${process.env.DB_PASSWORD}@img-data.n7vgx.mongodb.net/img-db?retryWrites=true&w=majority&appName=img-data`
   )
   .then(() => {
     console.log("Database is connected successfully");
