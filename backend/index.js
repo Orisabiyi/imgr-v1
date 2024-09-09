6c44fcd4f9f97208a48c8423a78d12f472a4d984
Orisabiyi <orisabiyidavid@gmail.com>
2024-09-09 18:08:20 +0100

    Revert "Add mongodb as a dependency in package.json"
    This reverts commit 7c023b396fa14b77e161d61faf2e13b7d4e8dfeb.

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,9 @@
 require("dotenv").config();
 
 const express = require("express");
-const mongoose = require("mongoose");
-
 const app = express();
 
 app.get("/", (req, res) => res.status(200).json({ message: "Hello World" }));
-
-mongoose
-  .connect(
-    "mongodb+srv://orisabiyidavid:NVvmB3iETZG8X3BM@img-data.n7vgx.mongodb.net/?retryWrites=true&w=majority&appName=img-data"
-  )
-  .then(() => {
-    console.log("Database is connected successfully");
-
-    app.listen(process.env.PORT, () =>
-      console.log(`Server is running at port ${process.env.PORT}`)
-    );
-  });
+app.listen(process.env.PORT, () =>
+  console.log(`Server is running at port ${process.env.PORT}`)
+);
