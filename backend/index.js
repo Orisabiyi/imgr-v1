705e9e65ff094c8457afc66960e6cb5931febe42
Orisabiyi <orisabiyidavid@gmail.com>
2024-09-09 14:27:47 +0100

    Refactor backend index.js to use Express for server setup

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => res.status(200).json({ message: "Hello World" }));
+app.listen(process.env.PORT, () =>
+  console.log(`Server is running at port ${process.env.PORT}`)
+);
