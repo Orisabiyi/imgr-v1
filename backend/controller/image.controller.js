ca8c26ba9eb024a75547532e3de55d9eee2e12f5
Orisabiyi <orisabiyidavid@gmail.com>
2024-09-10 04:41:48 +0100

    Refactor image.controller.js to validate image path before uploading

--- backend/controller/image.controller.js
+++ backend/controller/image.controller.js
@@ -1,8 +1,12 @@
 const { uploader } = require("../models/imgcloudinary.model");
+const validateImagePath = require("../utils/validate.util");
 
 const uploadImage = async function (req, res) {
   try {
     const { imgPath } = req.body;
+    if (!validateImagePath(imgPath))
+      return res.status(400).json({ message: "Invalid Image Path" });
+
     const uploadImg = await uploader(imgPath);
 
     console.log(uploadImg);
