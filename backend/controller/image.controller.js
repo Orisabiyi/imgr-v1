bcd0f2e0245380bc5eedd988fae2f7602140a4dc
Orisabiyi <orisabiyidavid@gmail.com>
2024-09-10 05:01:30 +0100

    Refactor image.controller.js to store uploaded image data in the database

--- backend/controller/image.controller.js
+++ backend/controller/image.controller.js
@@ -1,4 +1,5 @@
 const { uploader } = require("../models/imgcloudinary.model");
+const Image = require("../models/image.model");
 const validateImagePath = require("../utils/validate.util");
 
 const uploadImage = async function (req, res) {
@@ -7,9 +8,23 @@ const uploadImage = async function (req, res) {
     if (!validateImagePath(imgPath))
       return res.status(400).json({ message: "Invalid Image Path" });
 
-    const uploadImg = await uploader(imgPath);
+    const {
+      width,
+      height,
+      asset_id: assetId,
+      secure_url: imgUrl,
+      created_at: uploadedAt,
+      display_name: displayName,
+    } = await uploader(imgPath);
 
-    console.log(uploadImg);
+    const storeImage = await Image({
+      width,
+      height,
+      assedId,
+      imgUrl,
+      uploadedAt,
+      displayName,
+    });
   } catch (error) {
     res.status(500).json({ error });
   }
