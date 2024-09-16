b9dc7b485473db4c01fe45c3e7d62bd64f85beeb
Orisabiyi <orisabiyidavid@gmail.com>
2024-09-16 09:18:09 +0100

    Refactor Homepage component to adjust position of floating Hello World section

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -24,7 +24,7 @@ function Homepage() {
     <main className="flex items-center justify-center h-screen bg-gray-200 relative overflow-hidden">
       <section
         className={`absolute top-[2rem] right-[-25rem] bg-white w-[15%] px-[2rem] py-[1rem] text-[1.6rem] border-l-orange-700 border-l-[.5rem] rounded-[.5rem] ${
-          error ? "right-[25rem]" : ""
+          error ? "right-[4rem]" : ""
         }`}
       >
         Hello World
