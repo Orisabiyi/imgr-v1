19bb87a199f72b8d9311bcada135649ce9f1377e
Orisabiyi <orisabiyidavid@gmail.com>
2024-09-17 10:20:10 +0100

    Refactor component styles to adjust width for Loading page

--- frontend/src/pages/Loading.jsx
+++ frontend/src/pages/Loading.jsx
@@ -59,7 +59,7 @@ function Loading() {
   return (
     <main className="h-screen flex items-center justify-center bg-gray-200">
       {isLoading ? (
-        <section className="rounded-xl px-[2rem] py-[3rem] shadow-md bg-white w-[85%] sm:w-[40%] lg:w-[30%]">
+        <section className="rounded-xl px-[2rem] py-[3rem] shadow-md bg-white w-[95%] sm:w-[80%] lg:w-[30%]">
           <h1 className="text-[1.6rem] mb-[2rem]">Uploading...</h1>
           <BarLoader color="#2563eb" data-testid="loader" />
         </section>
