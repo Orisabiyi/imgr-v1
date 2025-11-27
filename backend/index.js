3e4136d5403e8b3a0790f2bc665bfe3927fa799e
orisabiyi <orisabiyidavid@gmail.com>
2025-11-27 12:19:51 +0100

    chore: comment out MongoDB connection code

--- backend/index.js
+++ backend/index.js
@@ -17,14 +17,15 @@ app.get("/", (_, res) => res.status(200).json({ message: "Hello World" }));
 app.use("/api/image", imgRouter);
 
 // connecting to mongodb
-mongoose
-  .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@img-data.n7vgx.mongodb.net/img-db?retryWrites=true&w=majority&appName=img-data`
-  )
-  .then(() => {
-    console.log("Database is connected successfully");
+// mongoose
+//   .connect(
+//     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@img-data.n7vgx.mongodb.net/img-db?retryWrites=true&w=majority&appName=img-data`
+//   )
+//   .then(() => {
+//     console.log("Database is connected successfully");
 
-    app.listen(process.env.PORT, () =>
-      console.log(`Server is running at port ${process.env.PORT}`)
-    );
-  });
+//   });
+
+app.listen(process.env.PORT, () =>
+  console.log(`Server is running at port ${process.env.PORT}`)
+);
